f9dc1f60ae22171f9f7917f777db48ae9e5d69de
Wei Dai <greendaiwei@gmail.com>
2022-04-14 19:57:04 -0400

    creaated user login

--- model.py
+++ model.py
@@ -58,8 +58,8 @@ class Rating(db.Model):
     __tablename__ = "ratings"
 
     rating_id = db.Column(db.Integer,
-                         autoincrement=True,
-                         primary_key=True)
+                        autoincrement=True,
+                        primary_key=True)
     score = db.Column(db.Integer)
     movie_id = db.Column(db.Integer, db.ForeignKey("movies.movie_id"))
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
--- server.py
+++ server.py
@@ -30,7 +30,59 @@ def show_all_movies():
 def show_movie(movie_id):
 
     return render_template('movie_details.html', movie=crud.get_movie_by_id(movie_id))
+
+
+@app.route('/users')
+def show_all_users():
+
+    all_users = crud.get_users()
+
+    return render_template('all_users.html', all_users = all_users)
+
+@app.route('/users', methods=['POST'])
+def register_user():
+    """Create a new user."""
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    check = crud.get_user_by_email(email)
+    if check == None:
+        new_user = crud.create_user(email,password)
+        db.session.add(new_user)
+        db.session.commit()
+    else:
+        flash("Email already has an account. Try logging in instead.")
+
+    return redirect('/')
+
+@app.route('/login', methods=['POST'])
+def login():
+    '''sign into an account'''
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = crud.get_user_by_email(email)
+
+    if user == None:
+        flash("Email doesn't exist. Try creating a login instead.")
+    else:
+        if user.password == password:
+            session['user_id'] = user.user_id
+            flash(f"You're logged in!, {session['user_id']}")
+        else:
+            flash("Wrong password")
+
+    return redirect('/')
+
+
+@app.route('/users/<user_id>')
+def show_user(user_id):
+
+    return render_template('user_details.html', user=crud.get_user_by_id(user_id))
     
+
 
 
 if __name__ == "__main__":
