ad765ce1b5d798f7d888ce60c72cc205dd87dca8
Wei Dai <greendaiwei@gmail.com>
2022-04-14 20:01:06 -0400

    Added extra files

---
+++ crud.py
@@ -0,0 +1,60 @@
+"""CRUD operations."""
+
+from model import db, User, Movie, Rating, connect_to_db
+from datetime import datetime
+
+
+def create_user(email, password):
+    """Create and return a new user."""
+
+    user = User(email=email, password=password)
+
+    return user
+
+def create_movie(title, overview, release_date, poster_path):
+    """Create and return a new movie."""
+    movie = Movie(
+        title = title, overview = overview, 
+        release_date = release_date , poster_path = poster_path)
+    
+    return movie
+
+
+def get_movies():
+    """Return all movies."""
+
+    return Movie.query.all()
+
+
+def get_movie_by_id(movie_id):
+    '''finds a particular movie given an id'''
+
+    return Movie.query.get(movie_id)
+
+
+def get_users():
+    """Return all user."""
+
+    return User.query.all()
+
+
+def get_user_by_id(user_id):
+    '''finds a particular user given an id'''
+
+    return User.query.get(user_id)
+
+def get_user_by_email(email):
+    """Return a user by email."""
+
+    return User.query.filter(User.email == email).first()
+
+def create_rating(user, movie, score):
+    '''Create and return a new score'''
+
+    rating = Rating(score=score, movie=movie, user=user)
+
+    return rating
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
